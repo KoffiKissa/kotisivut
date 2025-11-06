0cc3c494b043d1528cc9cd99dbdff1f80bd8aa5c
KoffiKissa <arttu.elias.tuhkanen@gmail.com>
2025-11-06 17:05:34 +0200

    Lisättin terveys php

---
+++ terveys.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Terveys </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+
+<div>
+    <h2 class="container-fluid p-3 text-center bg-primary">Terveys</h2>
+</div>
+
+ <nav class="navbar navbar-expand-sm bg-secondary navbar-dark sticky-top">
+
+        <div class="container-fluid">
+
+            <a class="navbar-brand" href="index.html">
+                <img src="kuva1.jpg" alt="Avatar Logo" class="rounded-pill" style="width:80px;">
+            </a>
+
+            <ul class="navbar-nav">
+
+                <li class="nav-item">
+                    <a class="nav-link" href="terveys.php">Terveys</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Linkki 2</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Linkki 3</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Linkki 4</a>
+                </li>
+
+            </ul>
+
+        </div>
+
+    </nav>
+
+</body>
+
+<footer class="container-fluid text-center fixed-bottom">
+    
+    <p>Tekijä: Arttu Tuhkanen</p>
+    <p><a href="mailto:arttu.tuhkanen@edu.sakky.fi">arttu.tuhkanen@edu.sakky.fi</a></p>
+
+</footer>
+
+</html>
