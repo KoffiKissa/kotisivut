2e36504362c77ac07884bf6c4fc58e4e8bf256c2
KoffiKissa <arttu.elias.tuhkanen@gmail.com>
2025-12-03 13:40:53 +0200

    AI slop edition tietovisaan ja toimivuus ?????????

--- tietovisa.php
+++ tietovisa.php
@@ -1,3 +1,32 @@
+<?php
+$questions = [
+    [
+        "id" => 1,
+        "question" => "Monestako pisteestä Dartsissa yleensä aloitetaan?",
+        "options" => ["301", "501", "701"],
+        "correct" => 2
+    ],
+    [
+        "id" => 2,
+        "question" => "Montako pelaajaa jallkapallossa on kerralla kentällä?",
+        "options" => ["9", "10", "11", "13"],
+        "correct" => 3
+    ],
+    [
+        "id" => 3,
+        "question" => "Montako askelta ihmisen pitäisi keskimäärin kävellä vuorokaudessa?",
+        "options" => ["5000", "8000", "12500"],
+        "correct" => 2
+    ],
+    [
+        "id" => 4,
+        "question" => "Kuka voitti Dartsin maailmanmestaruuden vuonna 2025?",
+        "options" => ["Luke Littler", "Luke Humphries", "Nathan Aspinal"],
+        "correct" => 1
+    ]
+];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -43,102 +72,51 @@
     </div>
 </nav>
 
-<form action="tietovisa.php" method="post">
+<?php foreach ($questions as $q): ?>
+    <div class="container-md kysymys text-center">
+        <div class="kysymys text-center" id="question-<?php echo $q["id"]; ?>">
+            <h3><strong><?php echo $q["question"]; ?></strong></h3>
 
-    <div class="container-md text-center kysymys">
-        <h3>Kysymys 1</h3><br>
-        <p>Monestako pisteestä dartsissa yleensä aloitetaan?</p>
-        <button type="button" id="K1V1" value="kysymys1" class="kysymys" onclick="return tarkista('paska')">301</button><br>
-        <button type="button" id="K1V2" value="kysymys1" class="kysymys">501</button><br>
-        <button type="button" id="K1V3" value="kysymys1" class="kysymys">701</button><br>
+            <?php foreach ($q["options"] as $i => $opt): ?>
+                <button class="btn kysymys"
+                        onclick="answerQuestion(<?php echo $q['id']; ?>, <?php echo $i; ?>, <?php echo $q['correct']; ?>, this)">
+                    <?php echo htmlspecialchars($opt); ?>
+                </button><br>
+            <?php endforeach; ?>
+        </div>
     </div>
+<?php endforeach; ?>
 
-    <div class="container-md text-center kysymys">
-        <h3>Kysymys 2</h3><br>
-        <p>Montako pelaajaa jalkapallossa on yhtäaikaa kentällä?</p>
-        <button type="button" value="kysymys 1" class="kysymys">10</button><br>
-        <button type="button" value="kysymys 1" class="kysymys">12</button><br>
-        <button type="button" value="kysymys 1" class="kysymys">14</button><br>
-    </div>
+<script>
+let score = 0;
 
-    <div class="container-md text-center kysymys">
-        <h3>Kysymys 3</h3><br>
-        <p>Montako askelta ihminen kävelee keskimäärin päivässä?</p>
-        <button type="button" value="kysymys 1" class="kysymys">7500</button><br>
-        <button type="button" value="kysymys 1" class="kysymys">10000</button><br>
-        <button type="button" value="kysymys 1" class="kysymys">12500</button><br>
-    </div>
+function answerQuestion(qid, chosen, correct, button) {
+    let box = document.getElementById("question-" + qid);
 
-    <div class="container-md text-center kysymys">
-        <h3>Kysymys 4</h3><br>
-        <p>Väärin tai Oikein</p>
-        <button type="button" value="kysymys 1" class="kysymys">Kyssäri a vai o</button><br>
-        <button type="button" value="kysymys 1" class="kysymys">Kyssäri a vai o</button><br>
-        <button type="button" value="kysymys 1" class="kysymys">Kyssäri a vai o</button><br>
-    </div>
+    // Lock all buttons in this question
+    box.classList.add("locked");
 
-    <div class="container-md text-center kysymys">
-        <h3>Kysymys 5</h3><br>
-        <p>Väärin tai Oikein</p>
-        <button type="button" value="kysymys 1" class="kysymys">Kyssäri a vai o</button><br>
-        <button type="button" value="kysymys 1" class="kysymys">Kyssäri a vai o</button><br>
-        <button type="button" value="kysymys 1" class="kysymys">Kyssäri a vai o</button><br>
-    </div>
+    // Mark chosen answer
+    if (chosen === correct) {
+        button.classList.add("correct");
+        score++;
+    }
+    else {
+        button.classList.add("incorrect");
 
-    <div class="container-md text-center kysymys">
-        <h3>Kysymys 6</h3><br>
-        <p>Väärin tai Oikein</p>
-        <button type="button" value="kysymys 1" class="kysymys">Kyssäri a vai o</button><br>
-        <button type="button" value="kysymys 1" class="kysymys">Kyssäri a vai o</button><br>
-        <button type="button" value="kysymys 1" class="kysymys">Kyssäri a vai o</button><br>
-    </div>
+        // Show correct answer
+        let correctBtn = box.getElementsByTagName("button")[correct];
+        correctBtn.classList.add("correct");
+    }
 
-    <div class="container-md text-center kysymys">
-        <h3>Kysymys 7</h3><br>
-        <p>Väärin tai Oikein</p>
-        <button type="button" value="kysymys 1" class="kysymys">Kyssäri a vai o</button><br>
-        <button type="button" value="kysymys 1" class="kysymys">Kyssäri a vai o</button><br>
-        <button type="button" value="kysymys 1" class="kysymys">Kyssäri a vai o</button><br>
-    </div>
+    // Update score text
+    document.getElementById("score").innerText = "Tulos: " + score;
+}
+</script>
 
-    <div class="container-md text-center kysymys">
-        <h3>Kysymys 8</h3><br>
-        <p>Väärin tai Oikein</p>
-        <button type="button" value="kysymys 1" class="kysymys">Kyssäri a vai o</button><br>
-        <button type="button" value="kysymys 1" class="kysymys">Kyssäri a vai o</button><br>
-        <button type="button" value="kysymys 1" class="kysymys">Kyssäri a vai o</button><br>
-    </div>
-
-    <div class="container-md text-center kysymys">
-        <h3>Kysymys 9</h3><br>
-        <p>Väärin tai Oikein</p>
-        <button type="button" value="kysymys 1" class="kysymys">Kyssäri a vai o</button><br>
-        <button type="button" value="kysymys 1" class="kysymys">Kyssäri a vai o</button><br>
-        <button type="button" value="kysymys 1" class="kysymys">Kyssäri a vai o</button><br>
-    </div>
-
-    <div class="container-md text-center kysymys">
-        <h3>Kysymys 10</h3><br>
-        <p>Väärin tai Oikein</p>
-        <button type="button" value="kysymys 1" class="kysymys">Kyssäri a vai o</button><br>
-        <button type="button" value="kysymys 1" class="kysymys">Kyssäri a vai o</button><br>
-        <button type="button" value="kysymys 1" class="kysymys">Kyssäri a vai o</button><br>
-    </div>
-
-    <?php
-
-        function tarkista($vastaus) {
-            echo "<i>{$vastaus}</i><br>";
-        }
-
-        echo "<div class='container-md text-center kysymys'>
-        <h3>Tulokset</h3><br>
-        <button type=button value=tulos class=tulos>Katso tulokset!</button><br>
-        </div>";
-
-    ?>
-
-</form>
+<div class="container-md kysymys">
+    <div id="score">Tulos: 0</div>
+</div>
 
 </body>
 
